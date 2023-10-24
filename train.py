45b59bc267dab86aeff3d0614277b2aa4aa1b15c
ashkan <ashkan.mokarian@gmail.com>
2023-10-24 11:38:28 +0200

    Add validation logging

--- train.py
+++ train.py
@@ -54,6 +54,40 @@ def train(netG, netD, train_dl, optimG, optimD, L1_Loss, BCE_Loss):
     return Disc_loss_train, Gen_loss_train
 
 
+def eval(netG, netD, val_dl, L1_Loss, BCE_Loss):
+    Disc_loss_val = []
+    Gen_loss_val = []
+
+    netG.eval()
+    netD.eval()
+    with torch.no_grad():
+        for idx, (x, y) in enumerate(tqdm(val_dl)):
+            x = x.to(config.device)
+            y = y.to(config.device)
+
+            # Discriminator
+            y_fake = netG(x)
+            D_real = netD(x, y)
+            D_real_loss = BCE_Loss(D_real, torch.ones_like(D_real))
+            D_fake = netD(x, y_fake.detach())
+            D_fake_loss = BCE_Loss(D_fake, torch.ones_like(D_fake))
+            D_loss = (D_real_loss + D_fake_loss) / 2
+
+            Disc_loss_val.append(D_loss.item())
+
+            # Generator
+            D_fake = netD(x, y_fake)
+            G_fake_loss = BCE_Loss(D_fake, torch.ones_like(D_fake))
+            L1 = L1_Loss(y_fake, y) * config.L1_LAMBDA
+            G_loss = G_fake_loss + L1
+
+            Gen_loss_val.append(G_loss.item())
+
+    netG.train()
+    netD.train()
+    return Disc_loss_val, Gen_loss_val
+
+
 def main():
     SAVE_ROOT = os.path.join(config.OUTPUT_DIR, config.NAME)
     CKPT_ROOT = os.path.join(SAVE_ROOT, "ckpts")
@@ -130,11 +164,26 @@ def main():
         )
         global_step = epoch * len(disc_loss)
         writer.add_scalar(
-            "Loss/Train/Disc", sum(disc_loss) / float(len(disc_loss)), global_step
+            "Loss/Disc/Train", sum(disc_loss) / float(len(disc_loss)), global_step
         )
         writer.add_scalar(
-            "Loss/Train/Gen", sum(gen_loss) / float(len(gen_loss)), global_step
+            "Loss/Gen/Train", sum(gen_loss) / float(len(gen_loss)), global_step
         )
+
+        # Let's validate every 5 epochs.
+        if epoch % 5 == 0:
+            disc_loss, gen_loss = eval(netG, netD, val_dl, L1_Loss, BCE_Loss)
+            writer.add_scalar(
+                "Loss/Disc/Val", sum(disc_loss) / float(len(disc_loss)), global_step
+            )
+            writer.add_scalar(
+                "Loss/Gen/Val", sum(gen_loss) / float(len(gen_loss)), global_step
+            )
+            print(
+                f"Finished VALIDATION for epoch {epoch}/{config.NUM_EPOCHS}: "
+                f"average disc-loss:{sum(disc_loss)/float(len(disc_loss)):.6f} "
+                f"average gen-loss:{sum(gen_loss)/float(len(gen_loss)):.6f}"
+            )
 
         if (epoch) % config.LOG_FREQ == 0:
             save_checkpoint(
@@ -148,7 +197,7 @@ def main():
                 os.path.join(CKPT_ROOT, CHECKPOINT_DISC.format(epoch)),
             )
             y_gen = save_some_examples(
-                netG, val_dl, epoch, fileroot=SAVE_EXAMPLE_IMAGE_ROOT
+                netG, train_dl, epoch, fileroot=SAVE_EXAMPLE_IMAGE_ROOT
             )
             writer.add_image(
                 "Train", make_grid(y_gen, nrow=4, normalize=True), global_step
